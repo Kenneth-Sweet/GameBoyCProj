cccb47969b6beb275a286bdb5d4412cc28431978
Kenneth-Sweet <milandalosur@gmail.com>
2022-03-26 11:53:46 -0400

    sprite addition

--- main.asm
+++ main.asm
@@ -9,7 +9,9 @@
 ; Public variables in this module
 ;--------------------------------------------------------
 	.globl _main
-	.globl _printf
+	.globl _set_sprite_data
+	.globl _delay
+	.globl _SampleSmile
 ;--------------------------------------------------------
 ; special function registers
 ;--------------------------------------------------------
@@ -21,6 +23,8 @@
 ; ram data
 ;--------------------------------------------------------
 	.area _INITIALIZED
+_SampleSmile::
+	.ds 32
 ;--------------------------------------------------------
 ; absolute external ram data
 ;--------------------------------------------------------
@@ -46,16 +50,94 @@
 ; Function main
 ; ---------------------------------
 _main::
-;main.c:6: printf("Hello World");
-	ld	de, #___str_0
+;main.c:7: UINT8 currentSpriteIndex = 0;
+	ld	c, #0x00
+;main.c:8: set_sprite_data(0,2, SampleSmile);
+	ld	de, #_SampleSmile
 	push	de
-	call	_printf
+	ld	hl, #0x200
+	push	hl
+	call	_set_sprite_data
+	add	sp, #4
+;c:/gbdk/include/gb/gb.h:1447: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 2)
+	ld	(hl), #0x00
+;c:/gbdk/include/gb/gb.h:1520: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	hl, #_shadow_OAM
+;c:/gbdk/include/gb/gb.h:1521: itm->y=y, itm->x=x;
+	ld	a, #0x4e
+	ld	(hl+), a
+	ld	(hl), #0x58
+;main.c:11: SHOW_SPRITES;
+	ldh	a, (_LCDC_REG + 0)
+	or	a, #0x02
+	ldh	(_LCDC_REG + 0), a
+;main.c:12: while(1){
+00105$:
+;main.c:13: if(currentSpriteIndex==0){
+	ld	a, c
+	or	a, a
+;main.c:14: currentSpriteIndex=1;
+;main.c:17: currentSpriteIndex = 0;
+	ld	c, #0x01
+	jr	Z, 00103$
+	ld	c, #0x00
+00103$:
+;c:/gbdk/include/gb/gb.h:1447: shadow_OAM[nb].tile=tile;
+	ld	hl, #(_shadow_OAM + 2)
+	ld	(hl), c
+;main.c:20: delay(1000);
+	push	bc
+	ld	de, #0x03e8
+	push	de
+	call	_delay
 	pop	hl
-;main.c:7: }
-	ret
-___str_0:
-	.ascii "Hello World"
-	.db 0x00
+	pop	bc
+;c:/gbdk/include/gb/gb.h:1536: OAM_item_t * itm = &shadow_OAM[nb];
+	ld	de, #_shadow_OAM+0
+;c:/gbdk/include/gb/gb.h:1537: itm->y+=y, itm->x+=x;
+	ld	a, (de)
+	ld	(de), a
+	inc	de
+	ld	a, (de)
+	add	a, #0x0a
+	ld	(de), a
+;main.c:21: scroll_sprite(0,10,0);
+;main.c:23: }
+	jr	00105$
 	.area _CODE
 	.area _INITIALIZER
+__xinit__SampleSmile:
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x77	; 119	'w'
+	.db #0x77	; 119	'w'
+	.db #0x77	; 119	'w'
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x63	; 99	'c'
+	.db #0x41	; 65	'A'
+	.db #0x7f	; 127
+	.db #0x7f	; 127
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x77	; 119	'w'
+	.db #0x77	; 119	'w'
+	.db #0x77	; 119	'w'
+	.db #0x55	; 85	'U'
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x00	; 0
+	.db #0x7f	; 127
+	.db #0x7f	; 127
+	.db #0x7f	; 127
+	.db #0x41	; 65	'A'
+	.db #0x00	; 0
+	.db #0x00	; 0
 	.area _CABS (ABS)
